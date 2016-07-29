0b7f7cc0dfd6f1c5c6605d61aa97c9fced2b353f
skodi <epicken2@gmail.com>
2016-07-29 22:48:12 +0100

    initial 2

---
+++ Cornwall/src/uk/co/lnssolutions/Fortune/data/DataProcessorConstants.java
@@ -0,0 +1,9 @@
+package uk.co.lnssolutions.Fortune.data;
+
+public class DataProcessorConstants {
+
+	//public static String zipFolder = "/Users/eliotpicken/Documents/workspace/BetfairDataProcessor/source_data";
+	public static String zipFolder = "/Users/eliotpicken/git/FortuneData/Cornwall/source_data";
+			
+			//"/Users/eliotpicken/Documents/workspace/Cornwall/source_data";
+}
